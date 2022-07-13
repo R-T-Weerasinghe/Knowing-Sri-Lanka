6da11197a6661d890da725fc9813f5592e38f79e
R-T-Weerasinghe <ravindutharuka2001@gmail.com>
2022-07-13 13:46:57 +0530

    slider working correctly

--- Demos/multi-range-demo/slider-script.js
+++ Demos/multi-range-demo/slider-script.js
@@ -1,25 +1,59 @@
-function slider1(slider) {
+function slider_left(slider) {
     // to prevent thumb from going past the other
     slider.value=Math.min(slider.value,slider.parentNode.children[2].value-1);
     
+    // value conversion from min-max of slider to 0-100% for style properties
     let value=(100*(slider.value-slider.min)/(slider.max-slider.min));
-    let children = slider.parentNode.childNodes[1].childNodes;
-    children[1].style.width=value+'%';
+
+    // selecting slider-style-container div - then its children
+    var children = slider.parentNode.children[0].children;
+
+    // bg-left
+    children[0].style.width=value+'%';
+    //bg-middle
+    children[2].style.left=value+'%';
+    // thumb-left
+    children[3].style.left=value+'%';
+    // tt-left-div
     children[5].style.left=value+'%';
-    children[7].style.left=value+'%';children[11].style.left=value+'%';
-    children[11].childNodes[1].innerHTML=slider.value;
+    // tt-left-div-span
+    children[5].children[0].innerHTML=slider.value;
+
     return null;
 }
 
-function slider2(slider) {
+function slider_right(slider) {
     // to prevent thumb from going past the other
     slider.value=Math.max(slider.value,slider.parentNode.children[1].value-(-1));
 
+    // value conversion from min-max of slider to 0-100% for style properties
     let value=(100/(parseInt(slider.max)-parseInt(slider.min)))*parseInt(slider.value)-(100/(parseInt(slider.max)-parseInt(slider.min)))*parseInt(slider.min);
-    let children = slider.parentNode.childNodes[1].childNodes;
-    children[3].style.width=(100-value)+'%';
-    children[5].style.right=(100-value)+'%';
-    children[9].style.left=value+'%';children[13].style.left=value+'%';
-    children[13].childNodes[1].innerHTML=slider.value;
+
+    // selecting slider-style-container div - then its children
+    var children = slider.parentNode.children[0].children;
+
+    // bg-right
+    children[1].style.width=(100-value)+'%';
+    // bg-middle
+    children[2].style.right=(100-value)+'%';
+    // thumb-right
+    children[4].style.left=value+'%';
+    // tt-right-div
+    children[6].style.left=value+'%';
+    // tt-right-div-span
+    children[6].children[0].innerHTML=slider.value;
+
     return null;
+}
+
+function update_slider_styles() {
+    var left_sliders = document.getElementsByClassName('slider-left');
+    for(let i=0; i<left_sliders.length; i++) {
+        slider_left(left_sliders.item(i));
+    }
+
+    var right_sliders = document.getElementsByClassName('slider-right');
+    for(let j=0; j<right_sliders.length; j++) {
+        slider_right(right_sliders.item(j));
+    }
 }
